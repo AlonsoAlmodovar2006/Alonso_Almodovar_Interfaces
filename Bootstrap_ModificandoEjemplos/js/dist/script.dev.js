6a3221f8fa2e4680e407f5a3220da4b132b592a7
Alonso Almodóvar <alonsoalmodovar09@gmail.com>
2025-10-16 19:56:59 +0200

    Práctica de Bootstrap

---
+++ Bootstrap_ModificandoEjemplos/js/dist/script.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/* sidebar */
+(function () {
+  'use strict';
+
+  var tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+})();
---
+++ Bootstrap_ModificandoEjemplos/js/script.js
@@ -0,0 +1,8 @@
+/* sidebar */
+(() => {
+  'use strict'
+  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(tooltipTriggerEl => {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
